74df7fd82e0d246f30ea559ba912a7247ec6ed85
Susanna Smbatyan <117935038+Susannajr@users.noreply.github.com>
2024-12-23 11:03:56 +0400

    Update ltr390_test.py

--- ltr390_test.py
+++ ltr390_test.py
@@ -2,9 +2,20 @@ import time
 import board
 import adafruit_ltr390
 
-i2c = board.I2C()
-ltr = adafruit_ltr390.LTR390(i2c)
+class LTR390Sensor:
+    def __init__(self, i2c_bus=None):
+        self.i2c = i2c_bus if i2c_bus else board.I2C()
+        self.ltr390 = adafruit_ltr390.LTR390(self.i2c)
+        
 
-while True:
-    print("UV:", ltr.uvs, "\t\tAmbient Light:", ltr.light)
-    time.sleep(1.0)
+    
+    def read_uv_and_light(self):
+        uv_light = self.ltr390.uvs  
+        ambient_light = self.ltr390.light 
+        return  uv_light, ambient_light
+
+    def read_data(self):
+        uv_light, ambient_light = self.read_uv_and_light()
+        print(f"LTR390 Sensor -> UV Light Intensity: {uv_light}")
+        print(f"LTR390 Sensor -> Ambient Light Intensity: {ambient_light}")
+        print("*************************")
